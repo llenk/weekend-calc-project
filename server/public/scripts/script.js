66c301273d31b899c4f7504857b516ec8ef34e17
llenk <ellenkeal@gmail.com>
2018-04-20 17:40:27 -0500

    finished main mode

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -1,6 +1,62 @@
 console.log('javascript connected');
 $(document).ready(onReady);
 
+let operation = {
+    x: 0,
+    y: 0,
+    type: ''
+}
+
 function onReady() {
-    console.log('jQuery loaded');
+    $('#add').on('click', function() {
+        operation.type = 'Add';
+    });
+    $('#subtract').on('click', function() {
+        operation.type = 'Subtract';
+    });
+    $('#multiply').on('click', function() {
+        operation.type = 'Multiply';
+    });
+    $('#divide').on('click', function() {
+        operation.type = 'Divide';
+    });
+    $('#equals').on('click', updateOperations);
+    $('#clear').on('click', clearHistory);
+}
+
+function updateOperations() {
+    sendOperation();
+    //updateOperationHistory is inside the sendOperation call
+    operation.type = ''; //reset operation
+}
+
+function sendOperation() {
+    operation.x = $('#firstNumber').val();
+    operation.y = $('#secondNumber').val();
+    $.ajax({
+        method: 'POST',
+        url: '/operation',
+        data: operation
+    }).then(function(response) {
+        updateOperationHistory();
+    });
+    $('input').val('');
+}
+
+function updateOperationHistory() {
+    $.ajax({
+        method: 'GET',
+        url: '/operation-history'
+    }).then(function(response) {
+        $('#operationsList').empty();
+        response.forEach(appendToDOM)
+    });
+}
+function appendToDOM(item) {
+    $('#operationsList').append(`<p>${item}</p>`);
+}
+
+function clearHistory() {
+    operation.type = 'Delete';
+    sendOperation();
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,78 @@
+//APP SETUP
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT  = process.env.PORT || 5000;
-
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//OPERATION HISTORY
+let operationHistory = [];
+
+//REQUESTS
+app.post('/operation', (req, res) => {
+    doMath(req.body);
+    res.sendStatus(200);
+});
+app.get('/operation-history', (req, res) => {
+    res.send(operationHistory);
+});
+
+
+
+//MATH FUNCTIONS
+doMath = (operation) => {
+    //conversion to numbers
+    operation.x = Number(operation.x);
+    operation.y = Number(operation.y);
+    //
+    if (operation.type == 'Add') {
+        add(operation);
+    }
+    else if (operation.type == 'Subtract') {
+        subtract(operation);
+    }
+    else if (operation.type == 'Multiply') {
+        multiply(operation);
+    }
+    else if (operation.type == 'Divide') {
+        divide(operation);
+    }
+    else if (operation.type == 'Delete') {
+        deleteHistory();
+    }
+    else {
+        error();
+    }
+}
+add = (operation) => {
+    let result = operation.x + operation.y;
+    let calculation = `${operation.x} + ${operation.y} = ${result}`;
+    operationHistory.unshift(calculation);
+} 
+subtract = (operation) => {
+    let result = operation.x - operation.y;
+    let calculation = `${operation.x} - ${operation.y} = ${result}`;
+    operationHistory.unshift(calculation);
+} 
+multiply = (operation) => {
+    let result = operation.x * operation.y;
+    let calculation = `${operation.x} * ${operation.y} = ${result}`;
+    operationHistory.unshift(calculation);
+} 
+divide = (operation) => {
+    let result = operation.x / operation.y;
+    let calculation = `${operation.x} / ${operation.y} = ${result}`;
+    operationHistory.unshift(calculation);
+} 
+deleteHistory = () => {
+    operationHistory = [];
+}
+error = () => {
+    operationHistory.unshift('ERROR: Please specify operation type');
+}
+
+//SERVER
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
